eba7cc0ee35ee68a7f8cbf1798790c5786478ab7
fedor <swiks321@gmail.com>
2019-04-20 20:03:56 +0400

    Создание и разметка основного класса Main

--- maps.py
+++ maps.py
@@ -1,5 +1,7 @@
-from PyQt5.QtWidgets import QMainWindow
-from big_API_maps.maps_gui import Ui_MainWindow
+from PyQt5.QtWidgets import QMainWindow, QApplication
+from maps_gui import Ui_MainWindow
+
+import sys
 
 
 class GUI(QMainWindow, Ui_MainWindow):
@@ -8,5 +10,17 @@ class GUI(QMainWindow, Ui_MainWindow):
         self.setupUi(self)
 
 
+class Main:
+    def __init__(self):
+        self.app = QApplication(sys.argv)
+        self.gui = GUI()
+
+    def run(self):
+        self.gui.show()
+
+        sys.exit(self.app.exec_())
+
+
 if __name__ == '__main__':
-    gui = GUI()
+    main = Main()
+    main.run()
